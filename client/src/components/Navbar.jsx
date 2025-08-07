d5edc9926e7ea17c27213e6199d8544be2b4d894
jacintolutrezehue@gmail.com <jacintolutrezehue@gmail.com>
2025-08-07 17:29:12 +0800

    Update

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import NavLink from './ui/NavLink';
-import { routes } from '../router/routes';
+import React from 'react'
+import NavLink from './ui/NavLink'
+import { routes } from '../router/routes'
+import reactLogo from '../assets/react.svg'
 
 const NavBar = () => {
     return (
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+                    <img src={reactLogo} className="h-8 mr-3" alt="Logo" />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">ASP.NET CRUD</span>
                 </a>
                 <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
