3383a4982c38db4a7484da9c6c9cd2926f48a0e8
jacintolutrezehue@gmail.com <jacintolutrezehue@gmail.com>
2025-08-07 17:47:03 +0800

    Update

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -13,7 +13,12 @@ const NavBar = () => {
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">ASP.NET CRUD</span>
                 </a>
                 <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                    <ButtonLink to="/login">Login</ButtonLink>
+                    <ButtonLink
+                        to="/login"
+                        className="bg-gray-500 text-white rounded hover:bg-gray-600"
+                    >
+                        Login
+                    </ButtonLink>
                 </div>
                 <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
                     <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
